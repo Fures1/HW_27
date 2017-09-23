ff56c2e2360190bbb3fed877f4834e680c3772b0
Seyran Abdullayev <s.abdullayevjb@gmail.com>
2017-09-23 12:01:56 -0700

    first Commit

---
+++ src/test/java/core/Input_Console.java
@@ -0,0 +1,22 @@
+package core;
+
+import java.io.Console;
+import java.util.Arrays;
+
+public class Input_Console {
+    public static void main(String[] args) {
+
+
+        Console c = System.console();
+
+        if (c == null) {
+            System.err.println("Console is not available ");
+            System.exit(0);
+            //System.out.print("enter your first name: ");
+            String first_name=c.readLine("Enter your first name: ");
+            String last_name=c.readLine("Enter your last name: ");
+            System.out.println("Your full name is: "+first_name+" "+last_name);
+
+        }
+    }
+}
